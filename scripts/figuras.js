5824975f35038bdd2c7feb1c7fcfd002c0782db7
Emerson Villalta <reaperfocs@gmail.com>
2021-07-12 09:36:54 -0500

    Practica con funciones

--- scripts/figuras.js
+++ scripts/figuras.js
@@ -1,35 +1,42 @@
 // cuadrado, triangulo y circulo por defecto
 //cuadrado, lado del cuadrado
 console.group();
-const ladocuadrado = 4;
-console.log("El lado del cuadrado es "+ ladocuadrado+"cm");
-const perimetrocuadrado = ladocuadrado*4;
-console.log("El perimetro del cuadrado es "+ perimetrocuadrado+"cm");
-const areacuadrado = ladocuadrado*ladocuadrado;
-console.log("El area del cuadrado es "+ areacuadrado+"cm");
+//const ladocuadrado = 4;
+//console.log("El lado del cuadrado es "+ ladocuadrado+"cm");
+function perimetrocuadrado(lado) {
+    return lado*4+"cm";
+}
+function areacuadrado(lado) {
+    return lado*lado+"cm^2";
+}
 console.groupEnd();
 //triangulo, altura y lados 
 console.group();
-const ladotrianguloa = 3;
+/*const ladotrianguloa = 3;
 const ladotriangulobase = 4;
 const ladotrianguloc = 5;
 console.log("Los lados del triangulo son: "+ladotrianguloa +"cm,"+ladotriangulobase +"cm,"+ladotrianguloc +"cm");
-const perimetrotriangulo = ladotrianguloa+ladotriangulobase+ladotrianguloc;
-console.log("El perimetro del triangulo es "+ perimetrotriangulo+"cm");
-const alturatriangulo = 6;
-console.log("La altura triangulo es "+ alturatriangulo+"cm");
-const areatriangulo = (ladotriangulobase*alturatriangulo)/2;
-console.log("El area del triangulo es "+ areatriangulo+"cm");
+*/
+function perimetrotriangulo(a,b,c) {
+    return a+b+c+"cm";
+}
+function areatriangulo(base,altura) {
+    return (base*altura)/2+"cm^2";   
+}
 console.groupEnd();
 //circulo, radio definido
 console.group();
 const pi = Math.PI;
-const radiocirculo = 4;
-console.log("El radio del circulo: "+radiocirculo +"cm");
-const perimetrocirculo = 2*pi*radiocirculo;
-console.log("El perimetro del circulo es "+ perimetrocirculo+"cm");
-const areacirculo = pi*radiocirculo*radiocirculo;
-console.log("El area del circulo es "+ areacirculo+"cm");
+function diametrocirculo(radio) {
+    return radio*2+"cm";
+}
+function perimetrocirculo(radio) {
+    return 2*pi*radio+"cm";
+}
+function areacirculo(radio) {
+    return pi*radio*radio+"cm^2";
+}
+
 console.groupEnd();
 
 //const, funciones y 
