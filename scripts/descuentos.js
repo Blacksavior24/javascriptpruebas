84980d49a10072a27597de9ae542b0fd32ac91d2
Emerson Villalta <reaperfocs@gmail.com>
2021-08-07 02:29:37 -0500

    talle 2 cumplido y reto también

--- scripts/descuentos.js
+++ scripts/descuentos.js
@@ -7,8 +7,42 @@ function Calculardescuento(){
     if(resultado<0){
         resultado=resultado*-1;
     }
-    alert(resultado);
+    //alert(resultado);
     const salida = document.getElementById("Salida");
-    const value3 = salida.value;
-    salida = resultado;
+    salida.innerHTML = "Tú precio con descuento son: s/." + resultado;
+}
+function Coupon(){
+    const Coupon = document.getElementById("coupon");
+    /*const radioboton = document.createElement("input");
+    radioboton.setAttribute("type","radio");
+    const opciones = document.createTextNode(coupons);
+    const radioboton = document.createElement("Input");
+    Coupon.appendChild(radioboton);
+    const opcionesval = radioboton.value;
+    */
+    const inputcupon = document.getElementById("Inputcupon");
+    const coupons = inputcupon.value;
+
+    let descuento;
+    switch (coupons) {
+        case "abecedario":
+            descuento = 15;
+            break;
+        
+        case "descuento":
+            descuento = 30;
+            break;
+        
+        case "gratis":
+            descuento = 100;
+            break;
+        
+    }
+    const input1 = document.getElementById("Inputproducto");
+    const value1 = input1.value;
+
+    const resultado =  ((value1*(100-descuento))/100);
+    
+    const salida = document.getElementById("Salida");
+    salida.innerHTML = "Tú precio con descuento son: s/." + resultado;
 }
